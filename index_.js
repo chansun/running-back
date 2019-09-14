ced220d14d50bc814eb890310fb19adfa1513684
Chansun Park <chansun@chansuns-air.wireless-1x.unc.edu>
2019-09-14 00:40:29 -0400

    speed test

--- index_.js
+++ index_.js
@@ -29,21 +29,6 @@ t1.addEventListener('dblclick', function(e){
     e.preventDefault();  //cancel system double-click event
 });
 
-//t1.addEventListener("touchend", playHandler, false);
-/*
-function handlerFunction(event) {
-    alert("touch worked!");
-};
-*/
-
-
-
-
-
-
-
-
-
 
 let sound2 = "Tutorial";
 const voice2 = new SpeechSynthesisUtterance(sound2);
@@ -51,16 +36,11 @@ voice2.pitch = 1.0;
 voice2.volume = 1.0;
 voice2.rate = 1.0;
 let t2 = document.getElementById('tutorial');
-
 function tutorialHandler (e) {
     speechSynthesis.speak(voice2);
 };
 t2.addEventListener('click', tutorialHandler);
-/*
-t2.addEventListener('click', function(){
-    speechSynthesis.speak(voice2);
-});
-*/
+
 
 let sound3 = "Play Page";
 const voice3 = new SpeechSynthesisUtterance(sound3);
@@ -68,7 +48,6 @@ voice3.pitch = 1.0;
 voice3.volume = 1.0;
 voice3.rate = 1.0;
 let t3 = document.getElementById('body');
-
 function bodyHandler (e) {
     if (e.keyCode == 39) {
         location.href= 'play.html';
@@ -76,11 +55,3 @@ function bodyHandler (e) {
     }
 };
 t3.addEventListener('keyup', bodyHandler);
-/*
-t3.addEventListener('keyup', function(e){
-    if (e.keyCode == 39) {
-        location.href= 'play.html';
-        speechSynthesis.speak(voice3);
-    }
-});
-*/
--- play_.js
+++ play_.js
@@ -8,12 +8,14 @@ setTimeout(function() {
     //speechSynthesis.speak(voice1);
 }, 2000);
 
+
 let sound2 = "Back to the main menu";
 const voice2 = new SpeechSynthesisUtterance(sound2);
 voice2.pitch = 1.0;
 voice2.volume = 1.0;
 voice2.rate = 1.0;
 let t2 = document.getElementById('back');
+// document object in JS = the implementation of DOM document root! 
 
 // Single-click occurs if click is done once
 // Double-click occurs if click is done twice within 0.2 sec.
@@ -33,29 +35,11 @@ function backHandler (e) {
         clicks = 0;                         //after action performed, reset counter
     }
 };
-
-/*
-t2.addEventListener('click', function(){
-    clicks++;  //count clicks
-    if(clicks === 1) {
-        timer = setTimeout(function() {
-            speechSynthesis.speak(voice2);  //perform single-click action    
-            clicks = 0;                     //after action performed, reset counter
-        }, DELAY);
-    } 
-    else {
-        clearTimeout(timer);                //prevent single-click action
-        location.href= 'index.html';        //perform double-click action
-        speechSynthesis.speak(voice3);      //perform double-click action
-        clicks = 0;                         //after action performed, reset counter
-    }
-});
-*/
-
 t2.addEventListener('click', backHandler);
 t2.addEventListener('dblclick', function(e){
     e.preventDefault();  //cancel system double-click event
 });
+
 
 let sound3 = "Main Page";
 const voice3 = new SpeechSynthesisUtterance(sound3);
@@ -63,7 +47,6 @@ voice3.pitch = 1.0;
 voice3.volume = 1.0;
 voice3.rate = 1.0;
 let t3 = document.getElementById('body');
-
 function bodyHandler (e) {
     if (e.keyCode == 37) {
         location.href= 'index.html';
@@ -72,11 +55,34 @@ function bodyHandler (e) {
 };
 t3.addEventListener('keyup', bodyHandler);
 
-/*
-t3.addEventListener('keyup', function(e){
-    if (e.keyCode == 37) {
-        location.href= 'index.html';
-        speechSynthesis.speak(voice3);
-    }
+
+
+
+let count = 0;
+$("#col-left").click(function(e) {
+    count += 1;
 });
-*/
+$("#col-right").click(function(e) {
+    count += 1;
+});
+
+timerId = setInterval(function() {
+    let speed = $("#speed");
+    speed.empty();
+    speed.append(count + "/sec");
+    count = 0;
+}, 1000);
+
+$("#text").click(function(e) {
+    clearInterval(timerId);
+    alert("pause");
+    let speed = $("#speed");
+    speed.empty();
+    count = 0;
+    speed.append(count + "/sec");
+});
+
+
+
+
+
